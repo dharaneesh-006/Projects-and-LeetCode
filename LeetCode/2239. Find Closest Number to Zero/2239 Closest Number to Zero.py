bec199ee5c43c7ca44e28e6a28d6586fd637d108
Dharaneesh <137403057+dharaneesh-006@users.noreply.github.com>
2024-09-28 12:30:00 +0530

    Delete LeetCode/2239. Find Closest Number to Zero directory

--- LeetCode/2239. Find Closest Number to Zero/2239 Closest Number to Zero.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findClosestNumber(self, nums: List[int]) -> int:
-        num = nums[0]
-        for i in nums:
-            if abs(i) < abs(num):
-                num = i
-        if abs(num) in nums:
-            return abs(num)
-        return num
